3371f3b8109024d3b157e88c4f87f59a87e33ca3
Edgar Hakobyan <ehakobyan94@gmail.com>
2022-09-19 20:27:10 +0400

    Initial Commit

---
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIApp.swift
+//  TwitterSwiftUI
+//
+//  Created by Edgar on 19.09.22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
